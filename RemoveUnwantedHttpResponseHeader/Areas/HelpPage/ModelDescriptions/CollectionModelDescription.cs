cdfb4582c061c48b67fb59175975dcd53903f178
Lợi Nguyễn <quangloi2302@gmail.com>
2017-08-08 21:24:37 +0700

    first commit

---
+++ RemoveUnwantedHttpResponseHeader/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RemoveUnwantedHttpResponseHeader.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ RemoveUnwantedHttpResponseHeader/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RemoveUnwantedHttpResponseHeader.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
